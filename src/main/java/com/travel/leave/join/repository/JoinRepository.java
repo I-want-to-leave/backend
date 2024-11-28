4ec060b361f23f483bda03cd4aee5c8aeda07730
chungjeongsu <wjdwlgh2000@gmail.com>
2024-11-28 16:13:13 +0900

    refactor: 삭제 추가 커밋

--- src/main/java/com/travel/leave/join/repository/JoinRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travel.leave.join.repository;
-
-import com.travel.leave.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JoinRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
