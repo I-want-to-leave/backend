00dfe77023d69aec78353958c91c305e082a6143
chungjeongsu <wjdwlgh2000@gmail.com>
2024-12-20 02:15:02 +0900

    chore: timestamp

--- src/main/java/com/travel/leave/domain/schedule/service/mapper/TravelFactory.java
+++ src/main/java/com/travel/leave/domain/schedule/service/mapper/TravelFactory.java
@@ -14,6 +14,8 @@ public class TravelFactory {
     public Travel generate(TravelRequestDTO travelRequestDTO) {
         String rawImage = getRepresentativeImage(travelRequestDTO.image());
 
+        log.info(travelRequestDTO.toString());
+
         if(isImagePath(rawImage)){  //만약 민규에게 요청받은 이미지가 경로가 아니면(사진파일자체일시), 이미지 저장로직까지
             return Travel.builder()
                     .name(travelRequestDTO.title())
